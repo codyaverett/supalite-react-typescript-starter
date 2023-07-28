27f088833a8b45f7dff92ff139784ac187a89d30
Cody Averett <codyaverett@gmail.com>
2023-07-28 15:14:47 -0500

    fixes for module and hmr

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+if (module.hot) {
+    module.hot.accept();
+}
+
 function App() {
-    return <h1>Hello, world!</h1>;
+    return <h1>Hello, Dude!</h1>;
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
